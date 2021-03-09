06bfe5f79932bb6b044490442910f37c220fc48a
amartynov-pantus <WAcuSUQSe56HsjmZ>
2021-03-09 13:34:06 +0400

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,158 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="!cardShow" class="wrapper">
+      <div class="mail" >
+        <div class="cover" @click="cardShow=!cardShow"></div>
+        <div class="letter" ><h1 >открыть</h1></div>
+      </div>
+      <div class="subscription-text">
+        Вам письмо, откройте конверт :)
+      </div>
+    </div>
+    <Card v-if="cardShow"></Card>
+
+    <div @click="cardShow=!cardShow" style="color: #dedede; text-align: center">
+      open=============
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+
+import Card from "@/components/views/Card";
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {Card},
+  data() {
+    return{
+      cardShow: false,
+    }
+  },
+
+  methods:{
+
   }
 }
 </script>
 
 <style>
+@import "assets/normalize.css";
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
+
+body {
+  background: #302929;
+}
+
+.wrapper{
+  position: absolute; /* Absolute positioning */
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);  /* Move x,y */
+}
+
+.mail{
+  width: 300px;
+  height: 200px;
+  background: #ff8789;
+  position: relative;
+  transform-style: preserve-3d; /* Keep 3Dx effect */
+  border-bottom-left-radius: 20px; /* Lower left corner */
+  border-bottom-right-radius: 20px;
+  cursor: pointer; /* Mouse style */
+}
+/* Right */
+.mail:before{ /* add before */
+  content: '';
+  position: absolute;
+  top: 0;
+  right: 0;
+  border-left: 140px solid Transparent;
+  border-right: 150px solid #ff393c;
+  border-top: 100px solid Transparent;
+  border-bottom: 100px solid #ff393c;
+  z-index: 5; /* Cascading sort */
+  border-bottom-right-radius: 10px;
+}
+/* left */
+.mail:after{
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  border-left: 150px solid #ff5355;
+  border-right: 140px solid Transparent;
+  border-top: 100px solid Transparent;
+  border-bottom: 100px solid #ff5355;
+  z-index: 4;
+  border-bottom-left-radius: 10px;
+}
+
+.cover:before{ /* Above */
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  border-left: 150px solid transparent;
+  border-right: 150px solid transparent;
+  border-top: 100px solid #ff8789;
+  border-bottom: 100px solid transparent;
+  z-index: 6;
+  transform-origin: top;
+  transform: rotateX(0deg);
+  transition: all 1s 1s ease;
+}
+
+.letter{ /* Card */
+  position: absolute;
+  z-index: 3;
+  top: 0px;
+  left: 10px;
+  background: #dedede;
+  width: 280px;
+  height: 180px;
+  border-radius: 20px;
+  transition: all 1s ease; /* Transition time */
+}
+
+.letter h1{ /* @ symbol */
+  position: absolute;
+  top: 30%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+  font-size: 30px;
+  color: #798992;
+  font-weight: 900;
+  animation: blink 1s linear infinite;
+  animation-delay: 3s;
+  text-decoration-line: underline;
+}
+
+@keyframes blink{
+  0%{opacity: 0;}
+  50%{opacity: .5;}
+  100%{opacity: 1;}
+}
+
+.mail:hover .cover:before{
+  transform: rotateX(180deg); /* Flip the X axis */
+  transition: all 1s ease;
+}
+
+.mail:hover .letter{
+  top: -80px; /* Use top to move */
+  transition: all 1s 1s ease;
+}
+
+.subscription-text{
+  margin: 10px 0;
+  text-align: center;
+  font-size: 20px;
+  color: #798992;
+}
+
 </style>
---
+++ src/components/views/Card.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="container">
+    <div class="box">
+      fdfdfdf
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+
+  methods: {
+
+  },
+
+  mounted() {
+
+  }
+
+}
+</script>
+
+<style scoped>
+
+
+.container{
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.box {
+  background: url('~@/assets/logoCard.jpg') no-repeat center center;
+  background-size: contain;
+  border-style: solid;
+  border-width: 1px;
+  display: block;
+
+  -webkit-transition: width 2s, height 2s,  -webkit-transform 2s;
+  transition: width 2s, height 2s,  transform 2s;
+  animation: 1s ease-out 0s 1 rotateCard;
+  animation-fill-mode: forwards;
+
+
+}
+
+@keyframes rotateCard{
+  0%{
+    opacity: .5;
+    width: 100px;
+    height: 150px;
+  }
+  /*50%{opacity: .5;}*/
+  100%{
+    width: 533px;
+    height: 800px;
+      -webkit-transform: rotate(360deg);
+      transform: rotate(360deg);
+  }
+}
+
+
+/*.box:hover {*/
+/*  !*background-color: #FFCCCC;*!*/
+/*  width: 200px;*/
+/*  height: 200px;*/
+/*  -webkit-transform: rotate(360deg);*/
+/*  transform: rotate(360deg);*/
+
+/*}*/
+
+</style>
