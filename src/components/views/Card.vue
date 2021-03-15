e7d5ea83b8cf4b9e7760e1fa6d7b769f0186569d
amartynov-pantus <WAcuSUQSe56HsjmZ>
2021-03-15 15:54:09 +0400

    back cosmetic done

--- src/components/views/Card.vue
+++ src/components/views/Card.vue
@@ -20,15 +20,50 @@
         <div class="front">
 
                 <div class="logo" v-if="showRoll">
-                  <span v-bind:class="{ 'name1': showRoll }" >S</span>
-                  <span class="ampersand">&</span>
-                  <span class="name2">A</span>
+                  <span class="name1" >С</span>
+                  <span class="ampersand">и</span>
+                  <span class="name2">А</span>
                   <span class="date">05.06.2021</span>
                 </div>
 
 
         </div>
-        <div class="back">Back</div>
+        <div class="back">
+
+          <div class="back-logo">
+
+            <div class="back-logo__text" v-if="showRoll">
+
+              <span >София</span>
+              <span >и</span>
+              <span >Aндрей</span>
+
+            </div>
+
+          </div>
+          <div class="back-event">
+
+            <div class="back-event__date-box">
+              <div class="back-event__day">17</div>
+              <div class="back-event__month-year">
+                <div>ИЮНЯ</div>
+                <div>2020 ГОДА</div>
+              </div>
+
+            </div>
+            <div class="back-event__separator">|</div>
+            <div class="back-event__location-box">
+              <div>РЕСТОРАН (МРАМОРНЫЙ ЗАЛ)</div>
+              <div>САНАТОРИЙ "СИНЯЯ ПТИЦА" В 17.00</div>
+            </div>
+
+          </div>
+          <div class="back-person">
+            <span>Андрей и Мария, мы будем счастливы </span> <br>
+            <span>разделить с вами этот чудесный день!</span>
+          </div>
+
+        </div>
       </div>
       </transition>
 
@@ -69,6 +104,7 @@ export default {
 
 <style scoped>
 @import url(https://fonts.googleapis.com/css?family=Open+Sans);
+/*@import url('~@/assets/rosa-morena.ttf');*/
 .container{
   /*min-height: 100vh;*/
 }
@@ -136,12 +172,14 @@ label {
   align-items: center
 }
 
+
+
 .back {
-  background: #222;
+  background: url('~@/assets/textCard.jpg') no-repeat center center;
   color: #FFF;
   -webkit-transform: rotateX(180deg);
   transform: rotateX(180deg);
-  background-size: contain;
+  background-size: 99%;
   position: absolute;
   height: 100%;
   width: 100%;
@@ -185,7 +223,7 @@ input {
   width: 165px !important;
   height: 180px !important;
   font-family: Baskerville;
-  color: #575557;
+  color: #ff4f90;
   position: relative !important;
   margin: 0 auto;
 }
@@ -199,18 +237,28 @@ input {
   transition: transform 1s linear;
   transform-origin: top left;
   transform-style: preserve-3D;
+  font-family: "RosaMarena",serif;
+
 }
+
+
+
+
+
 
 .name1 {
   /*position: relative;*/
-  top: 0px;
-  left: 22px;
+  top: 26px;
+  left: 28px;
   -webkit-transition: width 2s, height 2s,  -webkit-transform 5s;
   /*transition: width 2s, height 2s,  transform 5s;*/
   /*animation: 2s ease-out 0s 1 animation-name;*/
   /*transition: transform 1s linear;*/
   /*transform-origin: top left;*/
   /*transform-style: preserve-3D;*/
+  /*src: url(~@/assets/rosa-morena.ttf);*/
+
+
 
 
 }
@@ -233,14 +281,16 @@ input {
 .name2 {
   /*position: relative;*/
   top: 72px;
-  left: 112px;
+  left: 77px;
+
 }
 
 .ampersand {
   font-style: italic;
   /*position: relative;*/
   top: 36px;
-  left: 60px;
+  left: 75px;
+
 }
 
 .date {
@@ -252,33 +302,80 @@ input {
 }
 
 
-@import url(https://fonts.googleapis.com/css?family=Akronim);
 
-body {
-  background: #ca8;
+.back-logo{
+  margin-top: 35%;
 }
 
-/*.fire {*/
-/*  margin: 50px auto;*/
-/*  width: 80%;*/
-/*  max-width: 1000px;*/
-/*  height: 200px;*/
+.back-logo__text{
+  font-family: "RosaMarena",serif;
+  color: #ff4f90;
+  font-size: 53px;
+  line-height: 40px;
+}
+.back-logo__text > span {
+  display: block;
+}
+.back-logo__text span:nth-child(2) {
+  height: 47px;
+}
 
-/*  background-position: center center;*/
-/*  background-size: 1000px 200px;*/
-/*  border-radius: 10px;*/
-/*  font-family: 'Akronim';*/
-/*  overflow: hidden;*/
-/*  text-align: center;*/
-/*  vertical-align: middle;*/
 
-/*}*/
-/*.fire:before {*/
-/*  content: '';*/
-/*  display: inline-block;*/
-/*  height: 100%;*/
-/*  vertical-align: middle;*/
-/*}*/
+.back-event{
+  margin-top: 15%;
+  font-family: 'AlmaticRegular', serif;
+  color: #ff84de;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 25px;
+}
+
+.back-event > div {
+  padding: 5px;
+}
+
+
+.back-event__date-box{
+
+  display: flex;
+  justify-content: center;
+  text-align: start;
+}
+
+.back-event__day{
+  font-family: 'OpenSansCondensed', serif;
+  font-size: 66px;
+  line-height: 51px;
+  margin-right: 10px;
+}
+
+.back-event__month-year{
+  margin: auto;
+}
+
+.back-event__location-box{
+  TEXT-ALIGN: start;
+
+}
+
+.back-event__month-year > div:nth-child(1) , .back-event__location-box > div:nth-child(1)  {
+  font-family: 'AlmaticBold', serif;
+}
+
+.back-event__separator{
+  font-size: 74px;
+  line-height: 62px;
+}
+
+.back-person{
+  font-family: 'CaveatRegular', serif;
+  font-size: 26px;
+  color: #ff84de;
+  margin-top: 10%;
+}
+
+
 
 .Blazing {
   color: #35ff14;
